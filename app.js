9be89eaff16f2f6ee2807706e997398eded74c0b
AnaMariaRoj <rojasgalvisanamaria@gmail.com>
2025-04-02 17:47:30 -0500

    appointment-appointments

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ document.getElementById('appointmentForm').addEventListener('submit', function(e
     };
 
     // Enviar los datos usando Fetch API
-    fetch('https://agendarcita-backend.onrender.com/appointment', {
+    fetch('https://agendarcita-backend.onrender.com/appointments', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
